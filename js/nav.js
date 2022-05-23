91860b6edb34488c0ce333763cde373e0c16f0f2
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-05-23 10:03:52 +0900

    add smoothscroll.js

--- js/nav.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-$(function () {
-  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
-    $('.menu , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
-  })
-});
---
+++ js/smoothscroll.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(scroll => {
+        scroll.addEventListener('click', (e) => {
+            e.preventDefault();
+            const href = scroll.getAttribute('href');
+
+            const targetElement = href === '#' || href === '' ? document.getElementsByTagName('html')[0] : document.getElementById(href.replace('#', ''));
+
+            const top = targetElement.getBoundingClientRect().top;
+            const offset = window.scrollY;
+            const gap = 0;
+            const target = top + offset - gap;
+
+            window.scrollTo({
+                top: target,
+                behavior: 'smooth',
+            });
+        });
+    })
+})
