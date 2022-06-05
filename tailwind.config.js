49f7be1cfb46faeef568cf38b3081c711f3a69ce
PCS <pcs.engineer.team@gmail.com>
2022-06-05 13:49:50 +0900

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,40 @@ module.exports = {
         './index.html',
     ],
 
-    // theme: {
-    //     extend: {
-    //         fontFamily: {
-    //             sans: ['"M PLUS Rounded 1c"', ...defaultTheme.fontFamily.sans],
-    //         },
-    //     },
-    // },
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    DEFAULT: '#595757'
+                },
+                yellow: {
+                    DEFAULT: '#f9bc00'
+                },
+                lime: {
+                    DEFAULT: '#7bbd39'
+                },
+                sky: {
+                    DEFAULT: '#00b7eb'
+                },
+                red: {
+                    DEFAULT: '#ec7058'
+                },
+                pink: {
+                    DEFAULT: '#eb6d7c'
+                },
+                orange: {
+                    DEFAULT: '#f1932c'
+                },
+                blue: {
+                    DEFAULT: '#036eb7'
+                },
+                green: {
+                    DEFAULT: '#13ae67'
+                },
+                rose: {
+                    DEFAULT: '#e7273e'
+                }
+            }
+        },
+    },
 };
