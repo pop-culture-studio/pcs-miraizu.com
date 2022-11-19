bde4773108a20c94f9ffb865a354443df465f7eb
PCS <pcs.engineer.team@gmail.com>
2022-11-20 07:23:12 +0900

    Update NoteRss.vue

--- resources/js/components/NoteRss.vue
+++ resources/js/components/NoteRss.vue
@@ -1,12 +1,17 @@
 <template>
   <ul>
-    <li v-for="item in items">
+    <li v-for="item in items" class="my-3">
       <a :href="item.link" class="text-2xl text-blue hover:underline" target="_blank">
         {{ item.title }}
       </a>
       <time class="ml-3 text-gray-500" :datetime="item.date + 'T' + item.time">
         {{ item.date }} ({{ item.diff }})
       </time>
+      <div class="w-auto sm:w-1/2 mt-2">
+        <a :href="item.link" target="_blank">
+          <img :src="item.thumbnail" :alt="item.title" :title="item.title" class="rounded shadow hover:opacity-70">
+        </a>
+      </div>
     </li>
   </ul>
 </template>
