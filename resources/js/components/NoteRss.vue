9d26ed8b3050b7d8e1fea88a4b8025a1c522009f
PCS <pcs.engineer.team@gmail.com>
2022-11-20 11:30:58 +0900

    Update NoteRss.vue

--- resources/js/components/NoteRss.vue
+++ resources/js/components/NoteRss.vue
@@ -12,7 +12,7 @@
           {{ item.date }} ({{ item.diff }})
         </time>
         <h3 class="my-2 text-2xl">
-          <a :href="item.link" class="text-blue" target="_blank">
+          <a :href="item.link" class="text-blue hover:underline" target="_blank">
             {{ item.title }}
           </a>
         </h3>
