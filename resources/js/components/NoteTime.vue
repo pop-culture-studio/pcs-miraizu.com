db6aca718c54a5e006ba2335796978ab2866b599
PCS <pcs.engineer.team@gmail.com>
2022-11-26 17:37:32 +0900

    Create NoteTime.vue

---
+++ resources/js/components/NoteTime.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  date: String,
+  time: String,
+})
+
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/ja'
+
+dayjs.extend(relativeTime)
+dayjs.locale('ja')
+</script>
+
+<template>
+  <time :datetime="`${date}T${time}`" class="text-gray-500">
+    {{ date }} ({{ dayjs(`${date} ${time}`).fromNow() }})
+  </time>
+</template>
