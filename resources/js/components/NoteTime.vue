0edcb041d4542932dc4cc4c6bdeb53d17b28d46d
PCS <pcs.engineer.team@gmail.com>
2022-11-26 19:43:50 +0900

    Update NoteTime.vue

--- resources/js/components/NoteTime.vue
+++ resources/js/components/NoteTime.vue
@@ -6,14 +6,16 @@ const props = defineProps({
 
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
 import 'dayjs/locale/ja'
 
 dayjs.extend(relativeTime)
+dayjs.extend(localizedFormat)
 dayjs.locale('ja')
 </script>
 
 <template>
-  <time :datetime="`${date}T${time}`" class="text-gray-500">
-    {{ date }} ({{ dayjs(`${date} ${time}`).fromNow() }})
+  <time :datetime="dayjs(`${date} ${time}`).format()" class="text-gray-500">
+    {{ dayjs(date).format('LL') }} ({{ dayjs(`${date} ${time}`).fromNow() }})
   </time>
 </template>
