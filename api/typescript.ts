731d4a873d1cd99b5e1b4ff8c0b844f1517bdbb6
PCS <pcs.engineer.team@gmail.com>
2022-12-11 21:30:02 +0900

    Update typescript.ts

--- api/typescript.ts
+++ api/typescript.ts
@@ -3,9 +3,9 @@ import type {VercelRequest, VercelResponse} from '@vercel/node';
 const url = 'https://d2ttuujo1i51fi.cloudfront.net/note_pcs_miraizu.json';
 
 export default async function (req: VercelRequest, response: VercelResponse) {
-    const res = await fetch(url);
+    const res: Response = await fetch(url);
 
-    const data = await res.json();
+    const data: string = await res.json();
 
     response.setHeader('Content-Type', 'application/json; charset=utf-8').send(data);
 }
