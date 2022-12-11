7f43bf42eba4657f52f7dfc3c0cdcc9b51873125
PCS <pcs.engineer.team@gmail.com>
2022-12-11 19:04:26 +0900

    Create note.go

---
+++ api/note.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+const url = "https://d2ttuujo1i51fi.cloudfront.net/note_pcs_miraizu.json"
+
+type NoteJson []struct {
+	Title        string `json:"title"`
+	Link         string `json:"link"`
+	Embed        string `json:"embed"`
+	Description  string `json:"description"`
+	Thumbnail    string `json:"thumbnail"`
+	CreatorImage string `json:"creatorImage"`
+	Date         string `json:"date"`
+	Time         string `json:"time"`
+	Diff         string `json:"diff"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	var data = &NoteJson{}
+
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.NewEncoder(w).Encode(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
