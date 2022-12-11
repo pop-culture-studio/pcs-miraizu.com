02f22708284698d04355727c6c0b485424ae9dc2
PCS <pcs.engineer.team@gmail.com>
2022-12-11 19:32:46 +0900

    Update note.go

--- api/note.go
+++ api/note.go
@@ -1,24 +1,13 @@
 package handler
 
 import (
-	"encoding/json"
+  "fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 const url = "https://d2ttuujo1i51fi.cloudfront.net/note_pcs_miraizu.json"
-
-type NoteJson []struct {
-	Title        string `json:"title"`
-	Link         string `json:"link"`
-	Embed        string `json:"embed"`
-	Description  string `json:"description"`
-	Thumbnail    string `json:"thumbnail"`
-	CreatorImage string `json:"creatorImage"`
-	Date         string `json:"date"`
-	Time         string `json:"time"`
-	Diff         string `json:"diff"`
-}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	res, err := http.Get(url)
@@ -27,20 +16,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer res.Body.Close()
 
-	var data = &NoteJson{}
+  json, err := io.ReadAll(res.Body)
 
-	decoder := json.NewDecoder(res.Body)
-	err = decoder.Decode(data)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-	err = json.NewEncoder(w).Encode(data)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	fmt.Println(w, string(json))
 }
