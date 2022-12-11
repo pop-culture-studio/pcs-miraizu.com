6352f21e1f78c1424a1be5ff4aea6eadcb70677a
PCS <pcs.engineer.team@gmail.com>
2022-12-11 21:09:43 +0900

    node

--- api/node.js
+++ api/node.js
@@ -1,9 +1,8 @@
-import type {VercelRequest, VercelResponse} from '@vercel/node';
 import fetch from 'node-fetch';
 
 const url = 'https://d2ttuujo1i51fi.cloudfront.net/note_pcs_miraizu.json';
 
-export default async function (req: VercelRequest, response: VercelResponse) {
+export default async function (req, response) {
     const res = await fetch(url);
 
     const data = await res.json();
