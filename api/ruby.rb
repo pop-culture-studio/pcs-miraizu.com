d9d4e16376034ce1c8985e086cad9b209e11a601
PCS <pcs.engineer.team@gmail.com>
2022-12-12 13:17:49 +0900

    Update ruby.rb

--- api/ruby.rb
+++ api/ruby.rb
@@ -1,10 +1,8 @@
-require 'net/http'
-require 'uri'
 
 Handler = Proc.new do |req, res|
   url = 'https://d2ttuujo1i51fi.cloudfront.net/note_pcs_miraizu.json'
 
   res.status = 200
   res['Content-Type'] = 'application/json; charset=utf-8'
-  res.body = Net::HTTP.get(URI.parse(uri))
+  res.body = url
 end
