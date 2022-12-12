fb5e0441592f799d42342616af9916fd73001068
PCS <pcs.engineer.team@gmail.com>
2022-12-12 13:12:18 +0900

    wip

--- api/node.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://d2ttuujo1i51fi.cloudfront.net/note_pcs_miraizu.json';
-
-export default async function (req, response) {
-    const res = await fetch(url);
-
-    const data = await res.json();
-
-    response.setHeader('Content-Type', 'application/json; charset=utf-8').send(data);
-}
